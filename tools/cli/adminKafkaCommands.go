fcbf4ed9ed9c18fc82cff75a02dc820e665570dd
Quanzheng Long <longer@uber.com>
2018-11-17 11:17:51 -0800

    fix json encode

--- tools/cli/adminKafkaCommands.go
+++ tools/cli/adminKafkaCommands.go
@@ -183,7 +183,7 @@ Loop:
 				if err != nil {
 					fmt.Printf("failed to encode into json, err: %v", err)
 				}
-				output.WriteString(fmt.Sprintf("%v\n", jsonStr))
+				output.WriteString(fmt.Sprintf("%v\n", string(jsonStr)))
 			}
 		}
 	}
